a4d4baf90e605e3fa89ca9c03a554b299b27db77
giulianyfernandes <giulianysantos@gmail.com>
2024-03-20 01:44:25 +0000

    primeiro commit

---
+++ Untitled1.py
@@ -0,0 +1,12 @@
+print("ola mundo")
+
+print("Python has three numeric types: int, float, and complex")
+myValue=1
+
+print(myValue)
+print(type(myValue))
+print(type(myValue))
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
---
+++ hello-word.py
@@ -0,0 +1,11 @@
+print("Python has three numeric types: int, float, and complex")
+myValue=1
+print(type(myValue))
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=3.14
+print(myValue)
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+
